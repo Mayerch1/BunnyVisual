b45badddf83c878f0d0575db853c4319b17dc955
Christian Mayer <cj_mayer@outlook.de>
2018-03-12 20:02:17 +0100

    *Release .exe

--- BunnyVisual/GeneratedFiles/Release/moc_BunnyWindow.cpp
+++ BunnyVisual/GeneratedFiles/Release/moc_BunnyWindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_BunnyWindow_t {
-    QByteArrayData data[5];
-    char stringdata0[78];
+    QByteArrayData data[6];
+    char stringdata0[101];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,11 +35,13 @@ QT_MOC_LITERAL(0, 0, 11), // "BunnyWindow"
 QT_MOC_LITERAL(1, 12, 19), // "on_btnStart_clicked"
 QT_MOC_LITERAL(2, 32, 0), // ""
 QT_MOC_LITERAL(3, 33, 22), // "on_btnSettings_clicked"
-QT_MOC_LITERAL(4, 56, 21) // "on_btnDetails_clicked"
+QT_MOC_LITERAL(4, 56, 21), // "on_btnDetails_clicked"
+QT_MOC_LITERAL(5, 78, 22) // "on_btnSnapshot_clicked"
 
     },
     "BunnyWindow\0on_btnStart_clicked\0\0"
-    "on_btnSettings_clicked\0on_btnDetails_clicked"
+    "on_btnSettings_clicked\0on_btnDetails_clicked\0"
+    "on_btnSnapshot_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +51,7 @@ static const uint qt_meta_data_BunnyWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,11 +59,13 @@ static const uint qt_meta_data_BunnyWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x08 /* Private */,
-       3,    0,   30,    2, 0x08 /* Private */,
-       4,    0,   31,    2, 0x08 /* Private */,
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       4,    0,   36,    2, 0x08 /* Private */,
+       5,    0,   37,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -78,6 +82,7 @@ void BunnyWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 0: _t->on_btnStart_clicked(); break;
         case 1: _t->on_btnSettings_clicked(); break;
         case 2: _t->on_btnDetails_clicked(); break;
+        case 3: _t->on_btnSnapshot_clicked(); break;
         default: ;
         }
     }
@@ -109,13 +114,13 @@ int BunnyWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
