0a20fd8e1f3d4c536a688846b837cba1de69e487
Christian Mayer <cj_mayer@outlook.de>
2018-03-13 16:32:41 +0100

    *forgot Release-compiled exe

---
+++ BunnyVisual/GeneratedFiles/Release/moc_SettingsWindow.cpp
@@ -0,0 +1,199 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'SettingsWindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../SettingsWindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'SettingsWindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_SettingsWindow_t {
+    QByteArrayData data[21];
+    char stringdata0[507];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_SettingsWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_SettingsWindow_t qt_meta_stringdata_SettingsWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 14), // "SettingsWindow"
+QT_MOC_LITERAL(1, 15, 18), // "on_btnExit_clicked"
+QT_MOC_LITERAL(2, 34, 0), // ""
+QT_MOC_LITERAL(3, 35, 24), // "on_boxGridX_valueChanged"
+QT_MOC_LITERAL(4, 60, 1), // "i"
+QT_MOC_LITERAL(5, 62, 24), // "on_boxGridY_valueChanged"
+QT_MOC_LITERAL(6, 87, 28), // "on_boxFoodCount_valueChanged"
+QT_MOC_LITERAL(7, 116, 32), // "on_boxMaxColonySize_valueChanged"
+QT_MOC_LITERAL(8, 149, 32), // "on_boxInfectionProb_valueChanged"
+QT_MOC_LITERAL(9, 182, 31), // "on_boxStartBunnies_valueChanged"
+QT_MOC_LITERAL(10, 214, 28), // "on_boxSleepTime_valueChanged"
+QT_MOC_LITERAL(11, 243, 31), // "on_boxFoodDuration_valueChanged"
+QT_MOC_LITERAL(12, 275, 28), // "on_boxMaxHunger_valueChanged"
+QT_MOC_LITERAL(13, 304, 25), // "on_checkLoad_stateChanged"
+QT_MOC_LITERAL(14, 330, 25), // "on_checkSave_stateChanged"
+QT_MOC_LITERAL(15, 356, 24), // "on_checkLog_stateChanged"
+QT_MOC_LITERAL(16, 381, 26), // "on_checkNoLog_stateChanged"
+QT_MOC_LITERAL(17, 408, 24), // "on_checkCsv_stateChanged"
+QT_MOC_LITERAL(18, 433, 24), // "on_boxSave_returnPressed"
+QT_MOC_LITERAL(19, 458, 24), // "on_boxLoad_returnPressed"
+QT_MOC_LITERAL(20, 483, 23) // "on_boxCsv_returnPressed"
+
+    },
+    "SettingsWindow\0on_btnExit_clicked\0\0"
+    "on_boxGridX_valueChanged\0i\0"
+    "on_boxGridY_valueChanged\0"
+    "on_boxFoodCount_valueChanged\0"
+    "on_boxMaxColonySize_valueChanged\0"
+    "on_boxInfectionProb_valueChanged\0"
+    "on_boxStartBunnies_valueChanged\0"
+    "on_boxSleepTime_valueChanged\0"
+    "on_boxFoodDuration_valueChanged\0"
+    "on_boxMaxHunger_valueChanged\0"
+    "on_checkLoad_stateChanged\0"
+    "on_checkSave_stateChanged\0"
+    "on_checkLog_stateChanged\0"
+    "on_checkNoLog_stateChanged\0"
+    "on_checkCsv_stateChanged\0"
+    "on_boxSave_returnPressed\0"
+    "on_boxLoad_returnPressed\0"
+    "on_boxCsv_returnPressed"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_SettingsWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    1,  105,    2, 0x08 /* Private */,
+       5,    1,  108,    2, 0x08 /* Private */,
+       6,    1,  111,    2, 0x08 /* Private */,
+       7,    1,  114,    2, 0x08 /* Private */,
+       8,    1,  117,    2, 0x08 /* Private */,
+       9,    1,  120,    2, 0x08 /* Private */,
+      10,    1,  123,    2, 0x08 /* Private */,
+      11,    1,  126,    2, 0x08 /* Private */,
+      12,    1,  129,    2, 0x08 /* Private */,
+      13,    1,  132,    2, 0x08 /* Private */,
+      14,    1,  135,    2, 0x08 /* Private */,
+      15,    1,  138,    2, 0x08 /* Private */,
+      16,    1,  141,    2, 0x08 /* Private */,
+      17,    1,  144,    2, 0x08 /* Private */,
+      18,    0,  147,    2, 0x08 /* Private */,
+      19,    0,  148,    2, 0x08 /* Private */,
+      20,    0,  149,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void SettingsWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        SettingsWindow *_t = static_cast<SettingsWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_btnExit_clicked(); break;
+        case 1: _t->on_boxGridX_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->on_boxGridY_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_boxFoodCount_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->on_boxMaxColonySize_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->on_boxInfectionProb_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->on_boxStartBunnies_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->on_boxSleepTime_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_boxFoodDuration_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->on_boxMaxHunger_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->on_checkLoad_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_checkSave_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_checkLog_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->on_checkNoLog_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: _t->on_checkCsv_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->on_boxSave_returnPressed(); break;
+        case 16: _t->on_boxLoad_returnPressed(); break;
+        case 17: _t->on_boxCsv_returnPressed(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject SettingsWindow::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_SettingsWindow.data,
+      qt_meta_data_SettingsWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *SettingsWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *SettingsWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_SettingsWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int SettingsWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
